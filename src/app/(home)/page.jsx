38f7e7be70e21d62139d5d227bfb4522815c4c68
oleg <oleg.vozhakov.07@mail.ru>
2026-03-30 15:50:57 +0500

    added supabase base template for card

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,22 +1,46 @@
 import ProjectCard from '@/components/entities/project-card';
 import Container from '@/components/ui/Container';
 import { MOCK_CARDS } from '@/constants/mock_data';
+import { supabase } from '@/lib/supabase';
 
-const Home = () => {
+const Home = async () => {
+	const { data: projects, error } = await supabase
+		.from('projects')
+		.select(
+			`
+      *,
+      project_vacancies (
+        id,
+        role_title,
+        experience,
+        vacancy_skills (
+          skills ( name )
+        )
+      )
+    `,
+		)
+		.order('created_at', { ascending: false });
 	return (
 		<Container>
 			<div className='grid grid-cols-3 gap-x-8 gap-y-5'>
-				{MOCK_CARDS.map(item => (
-					<ProjectCard
-						key={item.id}
-						title={item.title}
-						description={item.description}
-						imageUrl={item.image_url}
-						vacancy={item.vacancy}
-						status={item.status}
-						isBookmarked={item.is_bookmarked}
-					/>
-				))}
+				{projects?.map(item => {
+					const firstVacancy = item.project_vacancies?.[0];
+					const skillsArray =
+						firstVacancy?.vacancy_skills?.map(vs => vs.skills.name) || [];
+					return (
+						<ProjectCard
+							key={item.id}
+							title={item.title}
+							description={item.description}
+							imageUrl={item.image_url}
+							status={item.status}
+							isBookmarked={item.is_bookmarked}
+							vacancyRole={firstVacancy?.role_title || 'Роль не указана'}
+							vacancyExperience={firstVacancy?.experience || 'Опыт не указан'}
+							skills={skillsArray}
+						/>
+					);
+				})}
 			</div>
 		</Container>
 	);
