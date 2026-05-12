f002d33225b177cd520a8ac7581812012873e582
oleg <oleg.vozhakov.07@mail.ru>
2026-05-12 22:21:13 +0500

    fixed links

--- src/shared/static/navigations.js
+++ src/shared/static/navigations.js
@@ -1,11 +1,9 @@
 import { Bookmark, Bell, MessageSquare } from 'lucide-react';
 
 export const NAV_ICONS = [
-	{ id: 1, name: 'Bookmarks', href: '/bookmarks', icon: Bookmark },
-	{ id: 2, name: 'Notifications', href: '/notifications', icon: Bell },
-	{ id: 3, name: 'Messages', href: '/messages', icon: MessageSquare },
+	{ id: 1, name: 'Bookmarks', href: '#', icon: Bookmark },
+	{ id: 2, name: 'Notifications', href: '#', icon: Bell },
+	{ id: 3, name: 'Messages', href: '#', icon: MessageSquare },
 ];
 
-export const NAV_LINKS = [
-	{ id: 1, label: 'Отклики', href: '/responses' },
-];
+export const NAV_LINKS = [{ id: 1, label: 'Отклики', href: '#' }];
